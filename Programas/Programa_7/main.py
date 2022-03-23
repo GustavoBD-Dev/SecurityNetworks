bb00c0cf9690fed239a2f9bc62caa66ff378d92b
Gustavo Blas Duran <gustavoblasduran1999@gmail.com>
2022-03-23 14:56:37 -0600

    Encrypt and Decrypt string with Feistel

--- Programas/Programa_7/main.py
+++ Programas/Programa_7/main.py
@@ -10,6 +10,11 @@ def changePosition(string):
     ans[0], ans[1], ans[2], ans[3] = ans[1], ans[3], ans[2], ans[0]
     return ''.join(ans)         # return the list as string
 
+def restartPosition(block):
+    ans = list(block)
+    ans[0], ans[1], ans[2], ans[3] = ans[3], ans[0], ans[2], ans[1]
+    return ''.join(ans)
+
 # This function increment the position letter
 def incrementLetter(string):
     # add char with ascii value to array
@@ -24,8 +29,23 @@ def incrementLetter(string):
                     aux.append(alphabet[j+1])
     return ''.join(aux)
 
+def decrementLetter(string):
+    # add char with ascii value to array
+    alphabet = [chr(_) for _ in range(65, 91, 1)]
+    aux = []
+    for i in range(len(string)):
+        for j in range(len(alphabet)):
+            if string[i] == alphabet[j]:
+                if j-1 < 0:
+                    aux.append(alphabet[(j+len(alphabet))-1])
+                else:
+                    aux.append(alphabet[j-1])
+    return ''.join(aux)
+
 def feistelEncrypt(string, nRounds):
+    stringList = []
     for i in range(nRounds):
+        print("\tROUND #{}".format(int(i)+1))
         stringList = []
         for i in range(len(string)):
             if i%2 == 1:
@@ -58,6 +78,48 @@ def feistelEncrypt(string, nRounds):
         print(stringList)
         string = stringList
         print()
+    return ''.join(stringList)
+
+def feistelDecrypt(stringEncoded, nRounds):
+    ans = []
+    for i in range(int(nRounds)):
+        print("\tROUND #{}".format(int(i)+1))
+        ans = []
+        for i in range(len(stringEncoded)):
+            if i%2 != 1:
+                ans.append(restartPosition(stringEncoded[i]))
+            else:
+                ans.append(stringEncoded[i])
+        print(ans)
+        
+        aux = []
+        for i in range(len(ans)):
+            if i%2 != 1:
+                aux.append(decrementLetter(ans[i]))
+            else:
+                aux.append(ans[i])
+        print(aux)
+
+        ans = []
+        for i in range(len(aux)):
+            if i%2 == 1:
+                ans.append(restartPosition(aux[i]))
+            else:
+                ans.append(aux[i])
+        print(ans)
+
+        aux = []
+        for i in range(len(ans)):
+            if i%2 == 1:
+                aux.append(decrementLetter(ans[i]))
+            else:
+                aux.append(ans[i])
+        print(aux)
+
+        stringEncoded = aux
+        ans = aux
+    return ''.join(aux)
+
 
 if __name__ == "__main__":    
     string = "ELECTRICIDADYELECTRONICA"
@@ -70,7 +132,14 @@ if __name__ == "__main__":
         stringSplit = stringSplitEachFourChars(string)
         print(stringSplit)
         print()
-        feistelEncrypt(stringSplit, 16)
+        ans = feistelEncrypt(stringSplit, 16)
+        print("String encoded: " , ans)
+
+        print("\nDECRYPT STRING -> {}".format(ans))
+        stringSplit = stringSplitEachFourChars(ans)
+        print(stringSplit)
+        decoded = feistelDecrypt(stringSplit, 16)
+        print("String decoded is: {}".format(decoded))
     else:
         i = 0
         # complete the string 
@@ -82,4 +151,10 @@ if __name__ == "__main__":
         stringSplit = stringSplitEachFourChars(string)
         print(stringSplit)
         print()
-        feistelEncrypt(stringSplit, 16)
+        ans = feistelEncrypt(stringSplit, 16)
+        print("String encoded: " , ans)
+        print("\nDECRYPT STRING -> {}".format(ans))
+        stringSplit = stringSplitEachFourChars(ans)
+        print(stringSplit)
+        decoded = feistelDecrypt(stringSplit, 16)
+        print("String decoded is: {}".format(decoded))
